cb06fb1f9ac62235773b499764e8c38fa04c7c92
Imanol Eizaguirre <imanol.eizaguirre@veeva.com>
2024-11-18 10:22:20 +0100

    chore(frontend): Remove frontend code

--- frontend/src/app/dto/stats.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface StatResponse {
-  /**
-   * Number of milliseconds that have been removed from current date in order
-   * to retrieve value .
-   */
-  periodDuration: number;
-
-  value: number;
-}
